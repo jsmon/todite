9a54ed58464e198b2804397ddd58f4855494485a
James Simon <jsmon.dev@gmail.com>
2021-02-13 14:03:16 +0000

    Converted to TypeScript and ignore /.vscode

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 
+import type { AppProps } from 'next/app';
+
 import '../styles/globals.css';
 
-const MyApp = ({ Component, pageProps }) => (
+const MyApp = ({ Component, pageProps }: AppProps) => (
     <Component {...pageProps} />
 );
 
