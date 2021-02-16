0a39f7a80198b1fc179035ddda5b4fb9d6326a13
James Simon <jsmon.dev@gmail.com>
2021-02-16 15:44:53 +0000

    Redirect to /todos if user is signed in

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
 
+import firebaseTypes from 'firebase';
+import { useAuthState } from 'react-firebase-hooks/auth';
+
+import firebase from '../firebase';
+
 import MetaData from '../components/MetaData';
 import Header from '../components/Header';
 import Buttons from '../components/Buttons';
@@ -8,14 +13,25 @@ import Creator from '../components/Creator';
 
 const Home = (): React.ReactElement<{
     children: React.ReactNode;
-}, 'div'> => (
-    <div>
-        <MetaData page="about" />
-        <Header page="about" />
-        <Buttons />
-        <About />
-        <Creator />
-    </div>
-);
+}, 'div'> => {
+    const isSSR = typeof window === 'undefined';
+
+    const auth = isSSR ? undefined : firebase.auth();
+    const [user] = isSSR ? [] : useAuthState(auth) as [firebaseTypes.User | undefined, boolean, firebaseTypes.auth.Error | undefined];
+
+    if (user) {
+        window.location.replace('/todos');
+    }
+
+    return (
+        <div>
+            <MetaData page="about" />
+            <Header page="about" />
+            <Buttons />
+            <About />
+            <Creator />
+        </div>
+    );
+};
 
 export default Home;
