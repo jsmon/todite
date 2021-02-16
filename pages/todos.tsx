741413c4c04e2321bd7307398a66348263dd2e90
James Simon <jsmon.dev@gmail.com>
2021-02-16 16:55:14 +0000

    Added metadata and header to todos page

--- pages/todos.tsx
+++ pages/todos.tsx
@@ -6,9 +6,13 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 
 import firebase from '../firebase';
 
+import MetaData from '../components/MetaData';
+import Header from '../components/Header';
 import Todos from '../components/Todos';
 
-const TodosPage = () => {
+const TodosPage = (): React.ReactElement<{
+    children: React.ReactNode;
+}, 'div'> => {
     const isSSR = typeof window === 'undefined';
     const auth = isSSR ? undefined : firebase.auth();
     const [user, loading] = isSSR ? [] : useAuthState(auth) as [firebaseTypes.User | undefined, boolean, firebaseTypes.auth.Error | undefined];
@@ -19,6 +23,8 @@ const TodosPage = () => {
         }
         return (
             <div>
+                <MetaData page="todos" />
+                <Header page="todos" />
                 <Todos />
                 <button>Sign Out</button>
             </div>
@@ -31,6 +37,8 @@ const TodosPage = () => {
 
     return (
         <div>
+            <MetaData page="todos" />
+            <Header page="todos" />
             <Todos user={user} />
             <button onClick={signOut}>Sign Out</button>
         </div>
