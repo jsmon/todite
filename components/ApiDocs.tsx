d8da88b1a00e926817b05143b60e9052701b08c3
James Simon <jsmon.dev@gmail.com>
2021-03-06 18:17:29 +0000

    Added link to CLI docs on about page

--- components/ApiDocs.tsx
+++ components/ApiDocs.tsx
@@ -9,11 +9,12 @@ const ApiDocs = (): React.ReactElement<{
 }, 'div'> => (
     <div id="for-developers" className="p-2">
         <h2 className="font-bold text-2xl">For Developers</h2>
-        <p>If you are a developer who would like to integrate Todite into your project, you can use these free resources:</p>
+        <p>If you are a developer who would like to integrate Todite into your project, you can use these free official resources:</p>
         <ul>
             <li><Link href="/docs/api-key"><a className="text-blue-600 hover:underline focus:underline">Your API Key</a></Link></li>
             <li><Link href="/docs/api/v1"><a className="text-blue-600 hover:underline focus:underline">REST API</a></Link></li>
             <li><Link href="/docs/npm/v0.1"><a className="text-blue-600 hover:underline focus:underline">npm package</a></Link></li>
+            <li><Link href="/docs/cli/v0.1"><a className="text-blue-600 hover:underline focus:underline">CLI</a></Link></li>
         </ul>
     </div>
 );
