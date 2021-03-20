7e3373267a6b1167c320bf0b95f6f0e597e6f743
James Simon <jsmon.dev@gmail.com>
2021-03-20 18:15:13 +0000

    Added install button

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,16 +8,38 @@ import MetaData from '../components/MetaData';
 import Header from '../components/Header';
 import Buttons from '../components/Buttons';
 import About from '../components/About';
+import Install from '../components/Install';
 import ApiDocs from '../components/ApiDocs';
 import Creator from '../components/Creator';
 
 const Home = (): React.ReactElement<{
     children: React.ReactNode;
 }, 'div'> => {
+    interface BeforeInstallPromptEvent extends Event {
+        readonly userChoice: Promise<{
+            outcome: 'accepted' | 'dismissed';
+            platform: string;
+        }>;
+
+        prompt(): Promise<void>;
+    }
+
     const [theme, setTheme] = useState<Theme>('system');
+    const [showInstallButton, setShowInstallButton] = useState(true);
+    const [deferredPrompt, setDeferredPrompt] = useState<BeforeInstallPromptEvent  | null>(null);
 
     useEffect(() => {
         (async () => {
+            window.addEventListener('beforeinstallprompt', e => {
+                const event = e as BeforeInstallPromptEvent;
+                event.preventDefault();
+
+                if (!deferredPrompt) return;
+
+                setDeferredPrompt(event);
+                deferredPrompt.prompt();
+            });
+
             const settings = await getSettings();
 
             setTheme(settings.theme);
@@ -34,12 +56,15 @@ const Home = (): React.ReactElement<{
         }
     }, [theme]);
 
+    const hideInstallButton = () => setShowInstallButton(false);
+
     return (
         <div>
             <MetaData page="about" />
             <Header page="about" />
             <Buttons />
             <About />
+            { showInstallButton && <Install deferredPrompt={deferredPrompt} hideInstallButton={hideInstallButton} setDeferredPrompt={setDeferredPrompt} /> }
             <ApiDocs />
             <Creator />
         </div>
